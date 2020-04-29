15eb719dcf5ca7ab2c6fb56c5b2f17dde4131cf1
javier samaca <samax80@hotmail.com>
2020-04-29 16:08:30 -0400

    Making Variable Random local to member  variable (gloabl)

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -56,7 +56,7 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
-
+	Random r;
 	bool shouldPlaySound = true;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Pfmproject00AudioProcessor)
