efd37486b55f55d801f3d750f215c34c4ba8dac9
javier samaca <samax80@hotmail.com>
2020-11-29 11:18:18 -0500

    Create_SLider_ add Slider contructor Method not audio passing through yet

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -12,12 +12,15 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-Pfmproject00AudioProcessorEditor::Pfmproject00AudioProcessorEditor (Pfmproject00AudioProcessor& p)
-    : AudioProcessorEditor (&p), processor (p)
+Pfmproject00AudioProcessorEditor::Pfmproject00AudioProcessorEditor(Pfmproject00AudioProcessor& p)
+    : AudioProcessorEditor(&p), processor(p)
 {
+
+    InitializeSlider(mGainSlider, Slider::TextBoxBelow, true, 50, 20, Slider::SliderStyle::LinearVertical, 0.0f, 1.0f, 0.01, 0.5f);
+
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize(200, 300);//here we set the size of the plugin window  
 }
 
 Pfmproject00AudioProcessorEditor::~Pfmproject00AudioProcessorEditor()
@@ -25,18 +28,32 @@ Pfmproject00AudioProcessorEditor::~Pfmproject00AudioProcessorEditor()
 }
 
 //==============================================================================
-void Pfmproject00AudioProcessorEditor::paint (Graphics& g)
+void Pfmproject00AudioProcessorEditor::paint(Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    auto backGroundColor = Colours::black;
+    g.fillAll(backGroundColor);
 
-    g.setColour (Colours::white);
-    g.setFont (18.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
 }
 
 void Pfmproject00AudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    mGainSlider.setBounds(getWidth() / 2 - 50, getHeight() / 2 - 75, 100, 150);
 }
+
+void Pfmproject00AudioProcessorEditor::InitializeSlider(Slider& mGainSlider, juce::Slider::TextEntryBoxPosition newPosition, bool isReadOnly, int textEntryBoxWidth, int textEntryBoxHeight, juce::Slider::SliderStyle newStyle, double newMin, double newMax, double newInt, double newValue)
+{
+    mGainSlider.setSliderStyle(newStyle);
+    mGainSlider.setTextBoxStyle(newPosition, isReadOnly, textEntryBoxWidth, textEntryBoxHeight);
+    mGainSlider.setRange(newMin, newMax, newInt);
+    mGainSlider.setValue(newValue);
+    addAndMakeVisible(mGainSlider);//this makes the child fo the component visible.
+}
+
+
+
+
+
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -16,20 +16,26 @@
 //==============================================================================
 /**
 */
-class Pfmproject00AudioProcessorEditor  : public AudioProcessorEditor
+class Pfmproject00AudioProcessorEditor : public AudioProcessorEditor
 {
 public:
-    Pfmproject00AudioProcessorEditor (Pfmproject00AudioProcessor&);
+    Pfmproject00AudioProcessorEditor(Pfmproject00AudioProcessor&);
     ~Pfmproject00AudioProcessorEditor();
 
     //==============================================================================
-    void paint (Graphics&) override;
+    void paint(Graphics&) override;
     void resized() override;
 
+    void InitializeSlider(Slider& mGainSlider, juce::Slider::TextEntryBoxPosition newPosition, bool isReadOnly, int textEntryBoxWidth, int textEntryBoxHeight, juce::Slider::SliderStyle newStyle, double newMin, double newMax, double newInt, double newValue);
 private:
+    /// <summary>
+    /// The Gain slider Object 
+    /// </summary>
+    Slider mGainSlider;
+
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Pfmproject00AudioProcessor& processor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Pfmproject00AudioProcessorEditor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Pfmproject00AudioProcessorEditor)
 };
